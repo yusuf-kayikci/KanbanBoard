a729d464636d462e239b5c3809c84c196949d67e
Yusuf Kayıkci <yusuf.kayikci@arutesolutions.com>
2019-11-14 04:51:39 +0300

    Created rest api.H2 implementation done.Drag and drop done.

--- kanbanboardui/src/components/addNewTaskModal.tsx
+++ kanbanboardui/src/components/addNewTaskModal.tsx
@@ -1,20 +1,69 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
+import { ITask, TaskStatus } from "./TaskBox";
+import { Post } from "../helper/requestHelper";
 
 interface IModalProps{
     show : boolean
     onHide : () => void
+    onCreatedTask : () => void
 }
 
+
+
+
+
+
+
 export const AddNewTaskModal : React.FC<IModalProps> = (props) => {
+    const [input , setInput] = useState<ITask>({
+        title : "",
+        completeDate : "",
+        content : "",
+        dueDate : "",
+        status : TaskStatus.TODO
+
+    });
+
+
     return(
         <Modal show={props.show} onHide ={props.onHide}  >
             <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
+                <Modal.Title>New Task</Modal.Title>
             </Modal.Header>
 
-            <Modal.Body>
-                <p>Modal body text goes here.</p>
+            <Modal.Body className="container">
+
+                    <div className="row form-margin">
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> Task Title </div>
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> <input type="text" 
+                        onChange={(evt : React.ChangeEvent<HTMLInputElement>) => setInput({...input , title : evt.target.value})} ></input></div>
+                    </div>
+                    <div className="row form-margin ">
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> Task Content </div>
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> 
+                        <input onChange={(evt : React.ChangeEvent<HTMLInputElement>) => setInput({...input , content : evt.target.value})}></input></div>
+                    </div>
+                    <div className="row form-margin ">
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> Due Date </div>
+                        <div className="col-md-6 col-lg-4 col-sm-6 col-xs-12"> 
+                        <input type="date" onChange={(evt : React.ChangeEvent<HTMLInputElement>) => setInput({...input , dueDate : evt.target.value})}  ></input></div>
+                    </div>
+                    <div className ="confirmNewTaskButtonWrapper">
+                        <button onClick={(evt : any) => {
+                            Post(input).then((data : ITask) => {
+                                console.log(data);
+                                alert(data.title  + " created...");
+                                props.onCreatedTask();
+                                
+                            }).finally(() => {
+                                props.onHide();
+                            })
+
+
+                        }}>Create Task</button>
+                    </div>
+                
             </Modal.Body>
 
             <Modal.Footer>
